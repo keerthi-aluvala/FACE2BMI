c92e659676effbbebe235f98ec81a39f59404191
keerthi aluvala <keerthialuvala7@gmail.com>
2023-03-22 14:18:48 +0530

    changed predict.py

--- predict.py
+++ predict.py
@@ -4,31 +4,25 @@ import librosa
 import soundfile
 import joblib 
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from sklearn.linear_model import Ridge
 
 model = joblib.load('model_joblib')
-def extract_feature(file_name, mfcc, chroma, mel):
-    with soundfile.SoundFile(file_name) as sound_file:
-        X = sound_file.read(dtype="float32")
-        sample_rate=sound_file.samplerate
-        if chroma:
-            stft=np.abs(librosa.stft(X))
-        result=np.array([])
-        if mfcc:
-            mfccs=np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
-            result=np.hstack((result, mfccs))
-        if chroma:
-            chroma=np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
-            result=np.hstack((result, chroma))
-        if mel:
-            mel=np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
-            result=np.hstack((result, mel))
+
+resnet = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+def extract_feature(file_name):
+    new_image = tf.keras.preprocessing.image.load_img(file_name, target_size=(224, 224))
+    new_image = np.array(tf.keras.preprocessing.image.img_to_array(new_image))
+    new_image = preprocess_input(new_image[np.newaxis, ...])
+    features = resnet.predict(new_image)
+    features = features.reshape(features.shape[0], -1)
+    prediction = ridge.predict(features)
+    result = prediction[0]
     return result
 #feature=extract_feature(str(sys.argv[1]), mfcc=True, chroma=True, mel=True)
-feature=extract_feature(str(sys.argv[1]), mfcc=True, chroma=True, mel=True)
-
-feature=feature.reshape(1,-1)
-# use model to predict
-
+feature=extract_feature(str(sys.argv[1]))
 
 
 prediction=model.predict(feature)
