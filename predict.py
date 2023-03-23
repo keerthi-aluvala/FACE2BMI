b89939ae8a8709bf37388c91a76dcc71a2094e4e
keerthi aluvala <keerthialuvala7@gmail.com>
2023-03-23 21:48:37 +0530

    changes in predict.py

--- predict.py
+++ predict.py
@@ -8,9 +8,10 @@ import tensorflow as tf
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 from sklearn.linear_model import Ridge
 
-model = joblib.load('model_joblib')
+model = joblib.load('G:\\sem VII\\MajorProject\\ourcode\\BMI\\bmi_predictor.model')
 
 resnet = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
 
 def extract_feature(file_name):
     new_image = tf.keras.preprocessing.image.load_img(file_name, target_size=(224, 224))
@@ -18,12 +19,13 @@ def extract_feature(file_name):
     new_image = preprocess_input(new_image[np.newaxis, ...])
     features = resnet.predict(new_image)
     features = features.reshape(features.shape[0], -1)
-    prediction = ridge.predict(features)
+    prediction = model.predict(features)
     result = prediction[0]
     return result
+    
 #feature=extract_feature(str(sys.argv[1]), mfcc=True, chroma=True, mel=True)
 feature=extract_feature(str(sys.argv[1]))
 
 
-prediction=model.predict(feature)
-print(prediction)
+
+print(feature)
